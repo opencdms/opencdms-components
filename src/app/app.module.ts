9a84e44c613230dd69fa1c9ac48fcaf9c8252936
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-24 12:42:12 -0800

    chore: code tidying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Injector, NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,6 +6,7 @@ import { TestComponent } from './components/test/test.component';
 
 /** All custom elements should be included here */
 const CUSTOM_ELEMENTS = [TestComponent];
+const CUSTOM_ELEMENTS_PREFIX = 'opencdms';
 
 @NgModule({
   declarations: [...CUSTOM_ELEMENTS],
@@ -13,13 +14,13 @@ const CUSTOM_ELEMENTS = [TestComponent];
   providers: [],
   bootstrap: [], // skip typical app-root bootratp
 })
-export class AppModule {
+export class AppModule implements DoBootstrap {
   constructor(private injector: Injector) {
     for (const component of CUSTOM_ELEMENTS) {
       const name = component.componentName;
       if (name) {
         const el = createCustomElement(component, { injector: this.injector });
-        customElements.define(`opencdms-${component.componentName}`, el);
+        customElements.define(`${CUSTOM_ELEMENTS_PREFIX}-${name}`, el);
       } else {
         console.warn(
           'Could not register component as static componentName not defined',
