ec8f5c1c54bbf5ec125780a3b450c7386c8e232e
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-01 12:20:51 -0800

    chore: include styles in dist output

--- scripts/concat-bundles.js
+++ scripts/concat-bundles.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs-extra");
 const concat = require("concat");
 
-/** Combine all output angular JS into single index.js file for easier consumption */
+/** Combine output angular JS into single index.js file for easier consumption */
 (async function concatBundles() {
   const buildDir = path.resolve(__dirname, "../dist", "webcomponents");
   const outDir = path.resolve(__dirname, "../components");
@@ -10,9 +10,19 @@ const concat = require("concat");
   fs.ensureDirSync(outDir);
   fs.emptyDirSync(outDir);
 
-  const files = ["runtime.js", "polyfills.js", "main.js"];
+  // List only files to include - this excludes lazy-loaded modules (currently all dev only)
+  const concatFiles = ["runtime.js", "polyfills.js", "main.js"];
 
-  const filePaths = files.map((filename) => path.resolve(buildDir, filename));
+  const filePaths = concatFiles.map((filename) => path.resolve(buildDir, filename));
 
   await concat(filePaths, `${outDir}/index.js`);
+
+  // Copy any additional standalone files
+  const standaloneFiles = ["styles.css"];
+
+  for (const filename of standaloneFiles) {
+    const src = path.resolve(buildDir, filename);
+    const dest = path.resolve(outDir, filename);
+    fs.copyFileSync(src, dest);
+  }
 })();
