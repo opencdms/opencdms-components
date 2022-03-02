8b3cf6479a809951055152c08515c73097f0957f
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-02 14:11:46 -0800

    feat: add station select value emitter

--- src/app/components/station-select/station-select.component.ts
+++ src/app/components/station-select/station-select.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { OpenCDMSApiService } from 'src/app/services/opencdms-api.service';
 import { OpenCDMSAPIModel } from 'src/models';
+
+type IStation = OpenCDMSAPIModel.components['schemas']['StationQueryResponse']['result'][0];
 
 @Component({
   selector: 'app-station-select',
   templateUrl: './station-select.component.html',
   styleUrls: ['./station-select.component.scss'],
-  encapsulation: ViewEncapsulation.ShadowDom,
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class StationSelectComponent implements OnInit {
-  dataIsLoading = true;
-  public stations: OpenCDMSAPIModel.components['schemas']['StationQueryResponse']['result'] = [];
+  public dataIsLoading = true;
+  public stations: IStation[] = [];
   public static componentName = 'station-select';
+
+  public selectedStations: { [station_id: string]: IStation } = {};
+
+  @Output() valueChanged = new EventEmitter<IStation[]>();
 
   constructor(private api: OpenCDMSApiService) {}
 
@@ -22,5 +28,14 @@ export class StationSelectComponent implements OnInit {
       this.stations = res.data.result;
     }
     this.dataIsLoading = false;
+  }
+
+  public toggleStationSelected(station: IStation) {
+    if (this.selectedStations.hasOwnProperty(station.station_id)) {
+      delete this.selectedStations[station.station_id];
+    } else {
+      this.selectedStations[station.station_id] = station;
+    }
+    this.valueChanged.next(Object.values(this.selectedStations));
   }
 }
