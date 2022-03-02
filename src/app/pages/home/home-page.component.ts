6713f28ab8b89287e60fbbaea57a459aaf6f4823
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-02 12:27:40 -0800

    feat: add dev home page

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { INavData } from '@coreui/angular';
+import { CUSTOM_ELEMENTS } from 'src/app/components/components.module';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  size = 'show' as any;
+  customElements: any[] = [];
+  navItems: INavData[] = [];
 
-  constructor() { }
+  public activeElement: any = {};
 
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.customElements = CUSTOM_ELEMENTS.map((element) => {
+      return { componentName: element.componentName };
+    });
+    this.navItems = this.customElements.map(({ componentName }) => ({
+      name: componentName,
+      url: `/home`,
+      linkProps: { queryParams: { componentName } },
+    }));
   }
 
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      const componentName = params.get('componentName');
+      this.activeElement =
+        this.customElements.find((el) => el.componentName === componentName) || this.customElements[0];
+    });
+  }
+
+  handleItemSelected(e: any) {
+    console.log('item selected', e);
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarModule, GridModule } from '@coreui/angular';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
   },
+  {
+    path: ':component',
+    component: HomePageComponent,
+  },
 ];
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ComponentsModule, SidebarModule, GridModule],
+  providers: [],
 })
 export class HomeModule {}
