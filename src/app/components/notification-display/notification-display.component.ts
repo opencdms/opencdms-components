eaa45e84e9e499d046ade545bd212fbd425e3fe5
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-09 13:44:48 -0800

    feat: add notification display and service

---
+++ src/app/components/notification-display/notification-display.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { NotificationService, INotfication } from 'src/app/services/notification.service';
+
+interface INotificationDisplay extends INotfication {
+  visible?: boolean;
+}
+
+@Component({
+  selector: 'app-notification-display',
+  templateUrl: './notification-display.component.html',
+  styleUrls: ['./notification-display.component.scss'],
+})
+export class NotificationDisplayComponent {
+  public notifications: INotificationDisplay[] = [];
+  private componentDestroyed$ = new Subject();
+
+  constructor(public notificationService: NotificationService) {}
+
+  public closeNotification(index: number) {
+    this.notifications[index].visible = false;
+  }
+
+  ngOnInit() {
+    this.notificationService.notifications$.pipe(takeUntil(this.componentDestroyed$)).subscribe((notification) => {
+      this.notifications.push({ ...notification, visible: true });
+    });
+  }
+
+  ngOnDestroy() {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.complete();
+  }
+}
---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface INotfication {
+  title: string;
+  text: string;
+  meta?: any;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  notifications$ = new Subject<INotfication>();
+
+  createNotification(notification: INotfication) {
+    this.notifications$.next(notification);
+    console.log('[NOTIFICATION]', notification);
+  }
+}
