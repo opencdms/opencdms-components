ebbbb5daee414760c170c84cd43a40b85353c898
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-09 13:45:33 -0800

    feat: add error notifications to opencdms api service

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SpinnerModule, TableModule, UtilitiesModule } from '@coreui/angular';
+import { SpinnerModule, TableModule, ToastModule, UtilitiesModule } from '@coreui/angular';
 import { StationSelectComponent } from './station-select/station-select.component';
 import { TestComponent } from './test/test.component';
 import { LoaderComponent } from './loader/loader.component';
+import { NotificationDisplayComponent } from './notification-display/notification-display.component';
 
+/** Components to export as standalone webcomponents */
 export const CUSTOM_COMPONENTS = [TestComponent, StationSelectComponent];
 
+/** Components shared locally but not exported */
+const CORE_COMPONENTS = [LoaderComponent, NotificationDisplayComponent];
+
 @NgModule({
-  declarations: [...CUSTOM_COMPONENTS, LoaderComponent],
-  imports: [CommonModule, FormsModule, SpinnerModule, TableModule, UtilitiesModule],
-  exports: [...CUSTOM_COMPONENTS],
+  declarations: [...CUSTOM_COMPONENTS, ...CORE_COMPONENTS],
+  imports: [CommonModule, FormsModule, SpinnerModule, TableModule, UtilitiesModule, ToastModule],
+  exports: [...CUSTOM_COMPONENTS, ...CORE_COMPONENTS],
 })
 export class ComponentsModule {}
