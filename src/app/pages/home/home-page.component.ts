eed2de6c815c0c61b9d8d35fad3edb87df797773
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-11 10:33:03 -0800

    feat: add dynamic webcomponent injection to home

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -5,6 +5,15 @@ import { Subscription } from 'rxjs';
 import { CUSTOM_COMPONENTS } from 'src/app/components/components.module';
 import { CUSTOM_DIALOGS } from 'src/app/dialogs/dialogs.module';
 
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CUSTOM_COMPONENTS_PREFIX } from 'src/app/register-webcomponents';
+
+interface IElementMeta extends INavData {
+  name: string;
+  url?: string;
+  linkProps?: { queryParams: { componentName: string } };
+}
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -12,13 +21,15 @@ import { CUSTOM_DIALOGS } from 'src/app/dialogs/dialogs.module';
 })
 export class HomePageComponent implements OnInit {
   size = 'show' as any;
-  navItems: INavData[] = [];
+  navItems: IElementMeta[] = [];
 
-  public activeElement: any = {};
+  public activeElement: IElementMeta = {} as any;
+
+  public activeElementHtml?: SafeHtml;
 
   private routeParams$: Subscription | undefined;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) {
     this.navItems = this.generateNavItems();
   }
 
@@ -26,6 +37,7 @@ export class HomePageComponent implements OnInit {
     this.routeParams$ = this.route.queryParamMap.subscribe((params) => {
       const componentName = params.get('componentName');
       this.activeElement = this.navItems.find((el) => el.name === componentName) || this.navItems[1];
+      this.injectActiveElementAsWebcomponent(this.activeElement.name as string);
       console.log({ componentName, activeElement: this.activeElement });
     });
   }
@@ -34,8 +46,15 @@ export class HomePageComponent implements OnInit {
     this.routeParams$?.unsubscribe();
   }
 
+  /** When an component has been selected dynamically inject code to render as a webcomponent */
+  private injectActiveElementAsWebcomponent(componentName: string) {
+    const tagName = `${CUSTOM_COMPONENTS_PREFIX}-${componentName}`;
+    console.log('injecting element', tagName);
+    this.activeElementHtml = this.sanitizer.bypassSecurityTrustHtml(`<${tagName}></${tagName}>`);
+  }
+
   /** Generate a list of entries for sidebar nav and component lookup */
-  private generateNavItems(): INavData[] {
+  private generateNavItems(): IElementMeta[] {
     const components = CUSTOM_COMPONENTS.map((element) => {
       return { componentName: element.componentName };
     });
@@ -50,7 +69,7 @@ export class HomePageComponent implements OnInit {
     ];
   }
 
-  private createComponentNavItem(componentName: string): INavData {
+  private createComponentNavItem(componentName: string): IElementMeta {
     return {
       name: componentName,
       url: `/home`,
