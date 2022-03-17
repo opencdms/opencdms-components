f752aeaedd2ea7622992b6f2d61946170ff3172e
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-16 21:21:47 -0700

    feat: add app version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import PACKAGE_JSON from '../../package.json';
 
 export const environment = {
+  appVersion: PACKAGE_JSON.version,
   production: false,
   componentServerEndpoint: 'https://opencdms-components-api-jnxkyuyk3q-ew.a.run.app',
   opencdmsServerEndpoint: 'https://api.opencdms.org/climsoft',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
