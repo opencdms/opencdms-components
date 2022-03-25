3300f838037debd3ad7c6c8b0f6e5ae92794f1e3
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-25 12:22:36 -0700

    chore: add alternate local endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
   appVersion: PACKAGE_JSON.version,
   production: false,
   componentServerEndpoint: 'https://opencdms-components-api-jnxkyuyk3q-ew.a.run.app',
+  // componentServerEndpoint: 'http://localhost:8000',
   opencdmsServerEndpoint: 'https://api.opencdms.org/climsoft',
 };
